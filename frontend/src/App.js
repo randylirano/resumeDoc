e18d22e01c3cccc1ff11357dc496e9ddf0d62c7b
randylirano <31170227+randylirano@users.noreply.github.com>
2021-11-22 16:12:24 -0800

    Added Login and SignUp routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import Landing from "./pages/Landing";
 import SweDashboard from "./pages/SweDashboard";
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/landing" element={<Landing />} />
         <Route exact path="/swe" element={<SweDashboard />} />
       </Routes>

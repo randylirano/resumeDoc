a621e9cd0c6589a55327ff504743d3c132764fe0
kezumah <kennedy.ezumah@gmail.com>
2021-11-29 11:24:16 -0800

    latest mod

---
+++ routes/swe.js
@@ -0,0 +1,39 @@
+// Written by Kennedy C. Ezumah
+// This route handles loading, creation, and deletion of swe resume data
+
+const express = require("express");
+const router = express.Router();
+const sweModuleDB = require("../db/sweModuleDB.js");
+
+// route for loading all swe resume data of a given user
+// an object with fields {userEmail : "XYZ"} is received in the body
+router.post("/", async (req, res) => {
+  // define a resume array to store objects of resumes
+  let body = req.body;
+  const resumes = await sweModuleDB.getAllResumes(body);
+  console.log("swe.js: module loaded...");
+  console.log(resume);
+
+  // send back data in JSON format to calling frontend script
+  res.json(resumes);
+});
+
+// route to create resume
+router.post("/create", async (req, res) => {
+  let data = { message: "No issues!" };
+  let statusCode = 200;
+
+  // define a resume array to store objects of resumes
+  let body = req.body;
+
+  try {
+    await sweModuleDB.createNewResume(body);
+  } catch (error) {
+    statusCode = 500;
+    data.message = error.message;
+  }
+  // send back response
+  res.status(statusCode).send(JSON.stringify(data));
+});
+
+module.exports = router;
